10ebf33b20f21fb4e864899d38f66429812ca3e7
Anthonycs96 <anthonycs.dev@gmail.com>
2024-07-26 00:46:39 -0500

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+  output: 'export',
   images: {
     unoptimized: true, // Para evitar advertencias de imagen, aunque es recomendable usar `next/image` para optimización
   },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Miportafolio/' : '',
+  trailingSlash: true,
 };
 
 export default nextConfig;
